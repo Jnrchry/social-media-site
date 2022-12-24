09b27cab3752a471b229baa509ff4e1555483f27
jnrchry <jnrchry@gmail.com>
2022-12-24 06:05:59 +0000

    commit

--- src/Components/Rightbarhome/Rightbarhome.js
+++ src/Components/Rightbarhome/Rightbarhome.js
@@ -3,11 +3,20 @@ import "./rightbarhome.scss";
 
 const Rightbarhome = () => {
   return (
-    <div className="rightbarhome">
-      <div className="bithdayContainer">
-        Hello
-        {/* <img src="assets/birthday/gift.png" alt="" className="birthdayImg" /> */}
+    <div className="rightbarHome">
+      <div className="birthdayContainer">
+        <img
+          src="./assets/birthdaygifts/gift.png"
+          alt=""
+          className="birthdayImg"
+        />
+
+        <span className="birthdayText">
+          <b>Felix Asare</b> and <b>other firends have a birthday today</b>
+        </span>
       </div>
+
+      <img src="./assets/ads/adv.jpg" alt="" className="rightAdvert" />
     </div>
   );
 };
